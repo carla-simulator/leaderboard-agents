b0fbd35d8623ff1872ffe672d179eef5bb9cefeb
Joel Moriana <joel.moriana@gmail.com>
2021-05-05 13:00:55 +0200

    share CARLA eggs

--- profiler_agent/make_docker.sh
+++ profiler_agent/make_docker.sh
@@ -13,19 +13,14 @@ mkdir -p ${SCRIPT_DIR}/.tmp
 mkdir -p ${SCRIPT_DIR}/.tmp/scenario_runner
 
 echo "Copying the submodules into the folder"
-cp -fr ${CARLA_ROOT}/PythonAPI  ${SCRIPT_DIR}/.tmp
 cp -fr ${SCRIPT_DIR}/../submodules/scenario_runner/srunner ${SCRIPT_DIR}/.tmp/scenario_runner/srunner  # Only srunner is really needed
 cp -fr ${SCRIPT_DIR}/../submodules/scenario_runner/requirements.txt ${SCRIPT_DIR}/.tmp/scenario_runner/requirements.txt  # And their requirements
 cp -fr ${SCRIPT_DIR}/../submodules/leaderboard/ ${SCRIPT_DIR}/.tmp
-cp -fr ${SCRIPT_DIR}/../submodules/map/ ${SCRIPT_DIR}/.tmp/map
 
-mv ${SCRIPT_DIR}/.tmp/PythonAPI/carla/dist/carla*-py3*.egg ${SCRIPT_DIR}/.tmp/PythonAPI/carla/dist/carla-leaderboard-py3x.egg
-
-echo "Copying the team code into the folder"
-cp -fr ${SCRIPT_DIR}/team_code/ ${SCRIPT_DIR}/.tmp/team_code
+cp ${SCRIPT_DIR}/entrypoint.sh .tmp/entrypoint.sh
 
 echo "Building the docker"
-docker build --force-rm -t map_agent -f Dockerfile ${SCRIPT_DIR}/.
+docker build --force-rm -t profiler_agent -f Dockerfile ${SCRIPT_DIR}/.
 
 echo "Removing the temporary folder"
 rm -fr ${SCRIPT_DIR}/.tmp
--- profiler_agent/run_docker.sh
+++ profiler_agent/run_docker.sh
@@ -9,4 +9,6 @@ docker run \
     --net=host \
     --volume=${SCRIPT_DIR}/results:/workspace/results:rw \
     --volume=${SCRIPT_DIR}/profiling:/workspace/profiling:rw \
+    --volume=${SCRIPT_DIR}/team_code:/workspace/team_code:rw \
+    --volume="${CARLA_ROOT}/PythonAPI":"/workspace/CARLA/PythonAPI":ro \
     profiler_agent:latest /bin/bash
