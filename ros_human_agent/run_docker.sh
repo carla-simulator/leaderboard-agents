62492de05b5ae6301103ebf5f45ed9880f717031
glopezdiest <glopez@cvc.uab.es>
2021-05-26 14:45:11 +0200

    READMEs and updated submodules to master

--- ros_human_agent/run_docker.sh
+++ ros_human_agent/run_docker.sh
@@ -5,10 +5,5 @@ ${SCRIPT_DIR}/docker-gui \
     -it \
     --rm \
     --net=host \
-    --volume=${SCRIPT_DIR}/../submodules/leaderboard:/workspace/leaderboard:rw \
-    --volume=${SCRIPT_DIR}/../submodules/scenario_runner:/workspace/scenario_runner:rw \
-    --volume=${SCRIPT_DIR}/../submodules/ros-bridge:/workspace/carla_ros_bridge/src:rw \
-    --volume=${SCRIPT_DIR}/team_code/src:/workspace/team_code/src:rw \
-    --volume=${SCRIPT_DIR}/team_code/ros_human_agent.py:/workspace/team_code/ros_human_agent.py:rw \
     --volume=${SCRIPT_DIR}/results:/workspace/results:rw \
     ros_human_agent:latest /bin/bash
