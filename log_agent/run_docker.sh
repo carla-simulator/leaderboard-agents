fb6020037c46484c49ed86916871809f840603b3
Guillermo <g.loepzdiest@gmail.com>
2022-09-28 12:40:20 +0200

    Added log agent

---
+++ log_agent/run_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+${SCRIPT_DIR}/docker-gui \
+    -it \
+    --rm \
+    --net=host \
+    --volume=${SCRIPT_DIR}/results:/workspace/results:rw \
+    --volume=${SCRIPT_DIR}/run_evaluation.sh:/workspace/leaderboard/scripts/run_evaluation.sh:rw \
+    --volume=${SCRIPT_DIR}/team_code/human_agent.py:/workspace/team_code/human_agent.py:rw \
+    --env HOST_RESULTS_PATH=${SCRIPT_DIR}/results \
+    --env ROUTES=/workspace/leaderboard/data/routes_training.xml \
+    --env DEBUG_CHALLENGE=1 \
+    --env ROUTES_SUBSET=0 \
+    log_agent:latest /bin/bash
