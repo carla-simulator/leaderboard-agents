f61b40ca92add8fac40e3bc99d5afc1e5b2dde17
Guillermo <g.loepzdiest@gmail.com>
2022-09-28 12:40:20 +0200

    Added log agent

---
+++ log_agent/run_evaluation.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+DOC_STRING="Run log agent."
+
+USAGE_STRING=$(cat <<- END
+Usage: $0 [-h|--help] [-m|--mode MODE] [-id|--test-id ID]
+The following modes are supported:
+    * log
+    * playback
+END
+)
+
+usage() { echo "$DOC_STRING"; echo "$USAGE_STRING"; exit 1; }
+
+MODE="log"
+ID="test"
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -m |--mode )
+      MODE=$2
+      if [ "$MODE" != "log" ] && [ "$MODE" != "playback" ]; then
+        usage
+      fi
+      shift 2 ;;
+    -id | --test-id )
+      ID=$2
+      shift 2 ;;
+    -h | --help )
+      usage
+      ;;
+    * )
+      shift ;;
+  esac
+done
+
+TEST_ROOT="/workspace/results/$ID"
+LOG_PATH="$TEST_ROOT/log.json"
+
+if [ $MODE = "log" ]; then
+  mkdir -m 777 -p $TEST_ROOT
+  echo "" > $LOG_PATH
+  export RECORD_PATH="$HOST_RESULTS_PATH/$ID"
+  export CHECKPOINT_ENDPOINT="$TEST_ROOT/results_log.json"
+elif [ -f "$LOG_PATH" ]; then
+  export CHECKPOINT_ENDPOINT="$TEST_ROOT/results_playback.json"
+else
+  echo "Could not playback the simulation from a non-existing file"
+  exit 1
+fi
+
+# Create configuration file
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo -e "mode: $MODE\nfile: $LOG_PATH" > $SCRIPT_DIR/human_agent_config.txt
+export TEAM_CONFIG=$SCRIPT_DIR/human_agent_config.txt
+
+python3 ${LEADERBOARD_ROOT}/leaderboard/leaderboard_evaluator.py \
+--routes=${ROUTES} \
+--routes-subset=${ROUTES_SUBSET} \
+--repetitions=${REPETITIONS} \
+--track=${CHALLENGE_TRACK_CODENAME} \
+--checkpoint=${CHECKPOINT_ENDPOINT} \
+--debug-checkpoint=${DEBUG_CHECKPOINT_ENDPOINT} \
+--agent=${TEAM_AGENT} \
+--agent-config=${TEAM_CONFIG} \
+--debug=${DEBUG_CHALLENGE} \
+--record=${RECORD_PATH} \
+--resume=${RESUME}
---
+++ log_agent/team_code/human_agent.py
@@ -0,0 +1,101 @@
+# Copyright (c) 2021 CVC.
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
+import json
+
+from leaderboard.autoagents.autonomous_agent import Track
+from leaderboard.autoagents.human_agent import HumanAgent as HumanAgent_
+from leaderboard.autoagents.human_agent import KeyboardControl as KeyboardControl_
+from leaderboard.autoagents.human_agent import HumanInterface
+from srunner.scenariomanager.carla_data_provider import CarlaDataProvider
+
+
+def get_entry_point():
+    return "HumanAgent"
+
+
+class HumanAgent(HumanAgent_):
+
+    def setup(self, path_to_conf_file):
+
+        self.track = Track.SENSORS
+
+        # Get the ego instance
+        self._player = None
+
+        for vehicle in CarlaDataProvider.get_world().get_actors().filter('vehicle.*'):
+            if vehicle.attributes['role_name'] == 'hero':
+                self._player = vehicle
+                break
+
+        if self._player is None:
+            raise ValueError("Couldn't find the ego vehicle")
+
+        self.agent_engaged = False
+        self.camera_width = 1280
+        self.camera_height = 720
+        self._side_scale = 0.3
+        self._left_mirror = False
+        self._right_mirror = False
+
+        self._hic = HumanInterface(
+            self.camera_width,
+            self.camera_height,
+            self._side_scale,
+            self._left_mirror,
+            self._right_mirror
+        )
+        self._controller = KeyboardControl(self._player, path_to_conf_file)
+        self._prev_timestamp = 0
+
+
+class KeyboardControl(KeyboardControl_):
+
+
+    def __init__(self, player, path_to_conf_file):
+        self._player = player
+        super().__init__(path_to_conf_file)
+
+    def _record_control(self):
+
+        velocity = self._player.get_velocity()
+        acceleration = self._player.get_acceleration()
+        transform = self._player.get_transform()
+
+        new_record = {
+            'control': {
+                'throttle': round(self._control.throttle, 2),
+                'steer': round(self._control.steer, 2),
+                'brake': round(self._control.brake, 2),
+                'hand_brake': self._control.hand_brake,
+                'reverse': self._control.reverse,
+                'manual_gear_shift': self._control.manual_gear_shift,
+                'gear': self._control.gear
+            },
+            'state': {
+                'velocity': {
+                    'x': round(velocity.x, 1),
+                    'y': round(velocity.y, 1),
+                    'z': round(velocity.z, 1),
+                    'value': round(velocity.length(), 1)
+                },
+                'acceleration': {
+                    'x': round(acceleration.x, 1),
+                    'y': round(acceleration.y, 1),
+                    'z': round(acceleration.z, 1),
+                    'value': round(acceleration.length(), 1)
+                },
+                'transform': {
+                    'x': round(transform.location.x, 1),
+                    'y': round(transform.location.y, 1),
+                    'z': round(transform.location.z, 1),
+                    'roll': round(transform.rotation.roll, 1),
+                    'pitch': round(transform.rotation.pitch, 1),
+                    'yaw': round(transform.rotation.yaw, 1)
+                }
+            }
+        }
+
+        self._log_data['records'].append(new_record)
