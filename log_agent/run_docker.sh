938d0df4f6fe665e0370dbc642ae7e11c96ec2b5
Guillermo <g.loepzdiest@gmail.com>
2022-09-29 09:07:13 +0200

    Added README

--- log_agent/run_docker.sh
+++ log_agent/run_docker.sh
@@ -7,8 +7,6 @@ ${SCRIPT_DIR}/docker-gui \
     --net=host \
     --volume=${SCRIPT_DIR}/results:/workspace/results:rw \
     --volume=${SCRIPT_DIR}/run_evaluation.sh:/workspace/leaderboard/scripts/run_evaluation.sh:rw \
-    --volume=${SCRIPT_DIR}/recorder_replayer.py:/workspace/team_code/recorder_replayer.py:rw \
-    --volume=${SCRIPT_DIR}/team_code/human_agent.py:/workspace/team_code/human_agent.py:rw \
     --env HOST_RESULTS_PATH=${SCRIPT_DIR}/results \
     --env ROUTES=/workspace/leaderboard/data/routes_training.xml \
     --env DEBUG_CHALLENGE=1 \
